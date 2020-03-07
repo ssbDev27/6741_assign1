8c38d81046efa5edfd8da299a634d2b14b4bddf6
Pulkit Ghai <39457346+PGhai@users.noreply.github.com>
2020-03-07 16:40:55 -0500

    Update query_executor.py

--- query_executor.py
+++ query_executor.py
@@ -1,3 +1,5 @@
+from platform import node
+
 import rdflib
 from rdflib import Namespace
 g = rdflib.Graph()
@@ -8,7 +10,19 @@ rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 
 g.parse("KG.ttl", format='n3')
 print (g)
-qres = g.query("""SELECT DISTINCT ?name WHERE { ?name rdf:type focu:Course .}""")
 
-for q in qres:
-    print(q)
+sparql1 = """SELECT ?description WHERE {
+?name focu:student_enrolled ?x .
+?x  focu:course_name_is ?course .
+?course focu:course_description ?description
+}"""
+
+# Infer the terms of subject in which NLP was offered
+sparql2 ="""
+Select ?terms WHERE{
+
+}
+"""
+q = g.query(sparql1)
+for qS in q:
+    print(qS)
