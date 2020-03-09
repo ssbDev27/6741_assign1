cbf0549a72bd2b48bbfb42cbcd58356af72ebbb1
Pulkit Ghai <39457346+PGhai@users.noreply.github.com>
2020-03-09 12:25:45 -0400

    Update query_executor.py

--- query_executor.py
+++ query_executor.py
@@ -11,50 +11,14 @@ rdf = Namespace("http://www.w3.org/1999/02/22-rdf-syntax-ns#")
 g.parse("KG.ttl", format='n3')
 print (g)
 
-sparql1 = """SELECT ?description WHERE {
-?name focu:student_enrolled ?x .
-?x  focu:course_name_is ?course .
-?course focu:course_description ?description
-}"""
-
-sparql2 = """
-
-SELECT (COUNT(*) as ?Triples) 
-WHERE 
-      { ?s ?p ?o } 
-"""
-sparql3= """
-Select ?topic ?link
-Where{ focudata:Applied_Artificial_Intelligence_ focu:course_topics ?topic .
-?topic rdf:seeAlso ?link .
-}"""
-# Infer the terms of subject in which NLP was offered
-sparql4 ="""Select ?course ?grade WHERE{
-<http://focu.io/data#Pulkit%Ghai> focu:student_enrolled ?x .
-?x focu:course_name_is ?course .
-?x focu:grades_achieved ?grade .
-}
-"""
-
-sparql5 ="""Select Distinct ?students   WHERE{
-?students focu:student_enrolled ?x .
-?x focu:course_name_is ?course.
-?course focu:course_topics focudata:distributed_systems .
-?x focu:grades_achieved ?grade .
- FILTER(?grade != "F") .
-}
-"""
-
-sparql6 ="""Select Distinct ?topic  WHERE{
-<http://focu.io/data#Pulkit%Ghai> focu:student_enrolled ?x .
-?x focu:course_name_is ?course.
-?course focu:course_topics ?topic .
-?x focu:grades_achieved ?grade .
- FILTER(?grade != "F") .
-}
-"""
+sparql1 = open("q1.txt", "r").read()
+sparql2 = open("q2.txt","r").read()
+sparql3= open("q3.txt","r").read()
+sparql4 = open("q4.txt","r").read()
+sparql5 =open("q5.txt","r").read()
+sparql6 =open("q6.txt","r").read()
 
 
-q = g.query(sparql2)
+q = g.query(sparql5)
 for qS in q:
     print(qS)
